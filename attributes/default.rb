2d5bfeed07458b15634cbe30df48f06c56d16eb5
Tim Smith <tsmith@chef.io>
2016-12-07 21:04:14 -0800

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
 default['yum']['docker']['baseurl'] = case node['platform']
-                                           when 'amazon'
-                                               'https://yum.dockerproject.org/repo/main/centos/6/'
-                                           when 'oracle'
-                                               "https://yum.dockerproject.org/repo/main/oraclelinux/#{node['platform_version'].to_i}/"
-                                           when 'fedora'
-                                               "https://yum.dockerproject.org/repo/main/fedora/#{node['platform_version'].to_i}/"
-                                           else
-                                               "https://yum.dockerproject.org/repo/main/centos/#{node['platform_version'].to_i}/"
-                                           end
+                                      when 'amazon'
+                                        'https://yum.dockerproject.org/repo/main/centos/6/'
+                                      when 'oracle'
+                                        "https://yum.dockerproject.org/repo/main/oraclelinux/#{node['platform_version'].to_i}/"
+                                      when 'fedora'
+                                        "https://yum.dockerproject.org/repo/main/fedora/#{node['platform_version'].to_i}/"
+                                      else
+                                        "https://yum.dockerproject.org/repo/main/centos/#{node['platform_version'].to_i}/"
+                                      end
 
 default['yum']['docker']['description'] = 'Docker Respository'
 default['yum']['docker']['gpgkey'] = 'https://yum.dockerproject.org/gpg'

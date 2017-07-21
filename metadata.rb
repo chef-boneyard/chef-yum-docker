21b4b87d9b15f62278f2d19ca9fb4665f45f89dd
Chase Bolt <chase.bolt@gmail.com>
2017-07-21 13:55:39 -0700

    [ci skip] updating CHANGELOG and bumping metadata
    Signed-off-by: Chase Bolt <chase.bolt@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Sets up the Docker yum repository'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '2.1.0'
+version '2.2.0'
 
 recipe 'chef-yum-docker::default', 'Sets up the Docker yum repository.'
 
